4523a98d2f884626e6677d971af03f96d991a130
Wojtek <kozaneckiwojtek@gmail.com>
2023-05-30 19:18:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, Route, Routes } from "react-router-dom"
+import Stopka from './stopka';
+import Baner from './baner';
+import Form from './form'
+import { Kursy } from "./kursy"
+import { Opis } from "./opis"
+
+
+
+function App() {
+  return (
+    <>
+
+      <Baner />
+
+      <Routes>
+        <Route path="/" element={<Opis />} />
+        <Route path="/kursy" element={<Kursy />} />
+        <Route path="form" element={<Form />} />
+      </Routes>
+
+      <Stopka />
+
+
+
+    </>
+  )
+}
+
+export default App;
+
+
